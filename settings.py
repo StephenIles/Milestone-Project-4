9799aa3ff8dc8235b990c6ec289f43aecca6a84a
Stephen Iles <stephenciles@gmail.com>
2024-11-30 14:04:16 +0000

    fix: template loading issue

--- settings.py
+++ settings.py
@@ -74,7 +74,7 @@ DATABASES = {
 # Static files configuration
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'receipe_website', 'recipes', 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files configuration
